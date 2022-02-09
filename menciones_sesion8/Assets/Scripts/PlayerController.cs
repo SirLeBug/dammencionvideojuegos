8020bc8fc8f577f3b755adf7af0ea843570c47af
SirLeBug <dani1sanz1frias@gmail.com>
2022-02-09 19:00:05 +0100

    Ejercicio 5

--- menciones_sesion8/Assets/Scripts/PlayerController.cs
+++ menciones_sesion8/Assets/Scripts/PlayerController.cs
@@ -6,18 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D playerRB;
     public float speed;
+    SpriteRenderer m_SpriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         float movimiento = Input.GetAxis("Horizontal");
         playerRB.velocity = new Vector2(speed * movimiento, playerRB.velocity.y);
+        //Debug.Log("Speed: " + speed);
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_SpriteRenderer.flipX = true;
+        }
         
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_SpriteRenderer.flipX = false;
+        }
+
     }
 
     // Update is called once per frame
